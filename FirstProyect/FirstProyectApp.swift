1afcbcd2ab680a0a9058883474f746807984cdd5
Luis Bustamante <luisebt995@gmail.com>
2024-03-26 18:43:02 +0100

    Initial Commit

---
+++ FirstProyect/FirstProyectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProyectApp.swift
+//  FirstProyect
+//
+//  Created by Luis on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstProyectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
